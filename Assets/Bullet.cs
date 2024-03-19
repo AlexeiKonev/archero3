542b7fe1d8b47f7505fa1b8d120f6ecbc1cfb1f4
AlexeiKonev <alexkonev777@gmail.com>
2024-03-19 10:54:58 +0300

    created scene

---
+++ Assets/Bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Bullet : MonoBehaviour
+{
+  public  Transform startPosition;
+
+  public  Transform target;
+  private  void Start()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    private void Reload( )
+    {
+        this.transform.position = startPosition.position;
+        Move();
+    }
+    private void Move()
+    {
+        transform.DOMove(target.position, 2)
+            .OnComplete(Reload);
+    }
+}
